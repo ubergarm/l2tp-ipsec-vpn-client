ada67137fd576a54e6a56eab08bdd57a97fd376a
Douglas Campbell <douglas@sharethis.com>
2018-12-07 13:29:42 -0800

    [first-] fix missing password

--- startup.sh
+++ startup.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+source /mnt/secrets/vpn.env
+set -e
+set -o nounset
+
 # template out all the config files using env vars
 sed -i 's/right=.*/right='$VPN_SERVER_IPV4'/' /etc/ipsec.conf
 echo ': PSK "'$VPN_PSK'"' > /etc/ipsec.secrets
 sed -i 's/lns = .*/lns = '$VPN_SERVER_IPV4'/' /etc/xl2tpd/xl2tpd.conf
 sed -i 's/name .*/name '$VPN_USERNAME'/' /etc/ppp/options.l2tpd.client
+
+VPN_PASSWORD=$(echo ${VPN_PASSWORD} | sed 's/\//\\\//g')
 sed -i 's/password .*/password '$VPN_PASSWORD'/' /etc/ppp/options.l2tpd.client
 
 # startup ipsec tunnel
@@ -16,5 +22,5 @@ ipsec statusall
 sleep 2
 
 # startup xl2tpd ppp daemon then send it a connect command
-(sleep 3 && echo "c myVPN" > /var/run/xl2tpd/l2tp-control) &
+(sleep 10 && echo "c myVPN" > /var/run/xl2tpd/l2tp-control) &
 /usr/sbin/xl2tpd -p /var/run/xl2tpd.pid -c /etc/xl2tpd/xl2tpd.conf -C /var/run/xl2tpd/l2tp-control -D
