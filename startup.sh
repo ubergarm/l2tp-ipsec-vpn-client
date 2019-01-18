696ac441ecb0cd4d13ed7e8fba674f1acfa792aa
John Leimgruber <leimgrub@gmail.com>
2019-01-18 18:59:40 +0000

    Give xl2tpd more time and exec it for clean exit

--- startup.sh
+++ startup.sh
@@ -22,5 +22,5 @@ ipsec --status
 sleep 3
 
 # startup xl2tpd ppp daemon then send it a connect command
-(sleep 5 && echo "c myVPN" > /var/run/xl2tpd/l2tp-control) &
-/usr/sbin/xl2tpd -p /var/run/xl2tpd.pid -c /etc/xl2tpd/xl2tpd.conf -C /var/run/xl2tpd/l2tp-control -D
+(sleep 7 && echo "c myVPN" > /var/run/xl2tpd/l2tp-control) &
+exec /usr/sbin/xl2tpd -p /var/run/xl2tpd.pid -c /etc/xl2tpd/xl2tpd.conf -C /var/run/xl2tpd/l2tp-control -D
